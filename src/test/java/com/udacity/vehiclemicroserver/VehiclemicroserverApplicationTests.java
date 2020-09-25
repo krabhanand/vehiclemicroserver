b7cfc2379889b939e95d30d4a09351035b8619a6
Kumar Abhishek Anand <kr.abh.anand@gmail.com>
2020-09-25 18:58:56 +0530

    Initial commit

---
+++ src/test/java/com/udacity/vehiclemicroserver/VehiclemicroserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.vehiclemicroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclemicroserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
